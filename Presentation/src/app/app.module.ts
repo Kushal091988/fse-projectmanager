7b0b6771b1fd9bbea291f59a9b805331a7fc02f4
Kushal <Kushalranty23@gmail.com>
2019-02-20 15:03:08 -0800

    Update: removed test component from module.ts

--- Presentation/src/app/app.module.ts
+++ Presentation/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './users/create-user/create-user.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateUserComponent,
-    TestComponent
   ],
   imports: [
     BrowserModule,
