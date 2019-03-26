620c662d7669b389f6bf55ee3a2c32a009a57b05
Kushal Kashyap <Kushal.Kashyap@Chubb.com>
2019-03-27 06:35:32 +0800

    update - added routing module and components

--- Presentation/src/app/app.module.ts
+++ Presentation/src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './users/create-user/create-user.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserComponent } from './users/user/user.component';
+import { ProjectComponent } from './projects/project/project.component';
+import { ProjectDetailComponent } from './projects/project-detail/project-detail.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateUserComponent,
+    UserListComponent,
+    UserComponent,
+    ProjectComponent,
+    ProjectDetailComponent,
+    TaskComponent,
+    TaskListComponent,
+    TaskDetailComponent,
   ],
   imports: [
     BrowserModule,
