59816967f92fdca6253fd12ec0904e9d6372e267
Kushal <Kushalranty23@gmail.com>
2019-07-20 04:31:32 -0700

    added tasks controller

---
+++ ProjectManager/WebApp/Api/TasksController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BusinessTier.Models;
+using DataAccess;
+
+namespace WebApp.Api
+{
+    [RoutePrefix("api/tasks")]
+    public class TasksController : ApiController
+    {
+        public TasksController()
+        {
+
+        }
+        private PMDbContext db = new PMDbContext();
+
+        [Route("getTasks")]
+        [HttpGet]
+        // GET: api/Users
+        public IHttpActionResult GetTasks()
+        {
+            return Json(db.Tasks);
+        } 
+    }
+}
